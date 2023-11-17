081b1d21d94ea65888780e1737c57e807813e803
lordecego <natanaeldeo.souza@gmail.com>
2023-11-17 15:34:52 -0300

    chore

---
+++ Weather/weather-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import WeatherApp from './components/WeatherApp/WeatherApp.jsx';
+
+const App = () => {
+
+
+  return (
+    <>
+      <WeatherApp/>
+    </>
+  )
+}
+
+export default App
